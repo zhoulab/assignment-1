86f3e9bc71e3ea0f1f6339a86691ba7d3dc5bddd
Victor Lin <vlin@acceleration.net>
2016-05-10 18:03:07 -0400

    Condense output, generalize functions
    * Generalized and renamed some functions, deleted others that were
    “duplicate” or not needed (replaced by list comprehensions).
    * Removed .anno from filename outputs, removed unnecessary information
    output from get_occurences_of_gene().
    * Buffed up documentation.

--- assignment.py
+++ assignment.py
@@ -2,134 +2,136 @@ import csv
 
 FILENAMES = ['Akdemir_p53_Untr.anno', 'Botcheve_IMR90.anno',
              'Younger_Human_ChIP.anno', 'Akdemir_p53_DOX.anno']
-VALUES = ['non-coding', 'Intergenic', 'intron', 'exon', 'promoter-TSS', 'TTS',
-          "5' UTR", "3' UTR"]
+SEARCH_VALUES = ['non-coding', 'Intergenic', 'intron', 'exon', 'promoter-TSS',
+                 'TTS', "5' UTR", "3' UTR"]
 
 
 def parse_file(filename):
     """
-        Returns parsed data of file in a data structure:
-            ({header1: value1-1, header2: value1-2, ...}
-             {header1: value2-1, header2: value2-2, ...} ...)
-        Where header(j) is the jth header in the file and
-        value(i)-(j) is the value under the jth header and ith row in the file.
+        Returns a generator object with each row in dictionary format.
+
+        Returns
+        -------
+        ({header1: value1-1, header2: value1-2, ...},
+         {header1: value2-1, header2: value2-2, ...}, ...)
+            Where header(j) is the jth header in the file and
+            value(i)-(j) is the value under the jth header and ith row in file.
     """
-    header = get_header(filename)
     with open(filename) as file:
         reader = csv.reader(file, delimiter='\t')
+        header = reader.next()
         for row in reader:
             yield dict(zip(header, (value for value in row)))
 
 
-def get_header(filename):
-    """Returns the elements of the first row in a list."""
-
-    with open(filename) as file:
-        reader = csv.reader(file, delimiter='\t')
-        return reader.next()
-
-
-def assignment_2():
+def assignment_2(col, values, files):
     """
         Data is fetched from get_count_lists and stored in a variable.
         Prints data in a formatted output.
-        Data structure:
-            {value1: [count_f1, count_f2, ...],
-             value2: [count_f1, count_f2, ...], ...}
+
+        Parameters
+        ----------
+        col: str
+            Column of .anno file to search under
+        values: list of str
+            Values to search for under col
+        files: list of str
+            Filenames of .anno files to search
+
+        Data Structures
+        ---------------
+        data: {value1: [count_f1, count_f2, ...],
+               value2: [count_f1, count_f2, ...], ...}
     """
-    data = dict(zip(VALUES, get_count_lists()))
-    print '%24s' % (''),
-    for name in FILENAMES:
-        print '%-24s' % (name,),
-    for value in VALUES:
+    data = dict(zip(values, [get_count(col, value, files)
+                             for value in values]))
+    print '\033[1m' + '%-24s' % ('Values'),
+    for filename in files:
+        print '%-24s' % (filename[:filename.index(".")],),
+    print '\033[0m',
+    for value in values:
         print '\n%-24s' % (value,),
         for count in data[value]:
             print '%-24s' % (count,),
 
 
-def get_count_lists():
+def get_count(col, value, files):
     """
-        For each value, returns a generator object of
-        list of counts for each file.
-
-        Data structure:
-            ([count_f1, count_f2, ...],
-             [count_f1, count_f2, ...], ...)
+        Returns
+        -------
+        (count_f1, count_f2, ...)
+            Generator object of counts for instances of *value* under *col*
+            (value does not have to be exact match)
     """
-    for value in VALUES:
-        count_list = get_count(value, 'Annotation')
-        yield count_list
+    for filename in files:
+        yield len(get_occurences(col, value, filename, False))
 
 
-def get_count(value, col):
+def get_occurrences_of_gene(gene_name, files):
     """
-        For each file, returns a generator object of counts
-        for instances of the value under a column.
-
-        Data structure:
-            (count_f1, count_f2, ...)
-    """
-    for filename in FILENAMES:
-        file_data = parse_file(filename)
-        count = 0
-        for row in file_data:
-            if value in row[col]:
-                count += 1
-        yield count
-
-
-def assignment_3(gene_name):
-    """
-        Data is fetched from get_data and stored in a variable
+        Data is fetched from get_occurences and stored in a variable
         then printed in a formatted output.
 
-        Data structure:
-            ((filename1, [row1, row2, ...]),
-             (filename2, [row1, row2, ...]), ...)
+        Parameters
+        ----------
+        gene_name: str
+            Gene name to search for under the column 'Gene Name'
+        files: list of str
+
+        Data Structures
+        ---------------
+        data: [(filename1, [row1, row2, ...]),
+               (filename2, [row1, row2, ...]), ...]
     """
-    data = get_data(gene_name)
+    data = [(filename, get_occurences('Gene Name', gene_name, filename))
+            for filename in files]
     for (filename, list_of_rows) in data:
-        header = get_header(filename)
-        print '\033[1m' + '%24s:' % (filename,),
+        print '\033[1m' + '%s:' % (filename[:filename.index(".")],),
         print '%i row(s) found with Gene Name=%r' % (len(list_of_rows),
                                                      gene_name),
         print '\033[0m'
-        for i, row in enumerate(list_of_rows):
-            # print row number for formatted list
-            print '\033[1m' + '%i:' % (i + 1,) + '\033[0m'
-            for key in header:
-                # special cases to bold Gene Name and truncate PeakID headers
-                if row[key] == gene_name:
-                    print '\033[1m' + '%24s: %s' % (key, row[key]) + '\033[0m'
-                elif 'PeakID' in key:
-                    print '%24s: %s' % ('PeakID', row[key])
-                else:
-                    print '%24s: %s' % (key, row[key])
-        print ''
+        if list_of_rows:
+            print '%-3s%-16s%-16s%-8s' % ("#", "Start", "End", "Length")
+
+        for (i, row) in enumerate(list_of_rows):
+            start = int(row["Start"])
+            end = int(row["End"])
+            print '%-3i%-16i%-16i%-8i' % (i + 1, start, end, end - start + 1)
+        print
 
 
-def get_data(gene_name):
+def get_occurences(col, value, filename, exact=True):
     """
-        For each file, returns generator object of tuple with
-        filename and list of rows containing matching gene name.
+        Parameters
+        ----------
+        col: str
+        value: str
+        filename: str
+        exact: bool
+            True:   will look for exact value under *col*
+            False:  will look for values under *col* that contain *value*
 
-        Data structure:
-            ((filename1, [row1, row2, ...]),
-             (filename2, [row1, row2, ...]), ...)
+        Returns
+        -------
+        [row1, row2, ...]
+            List of rows containing matching *value* under *col*.
     """
-    for filename in FILENAMES:
-        file_data = parse_file(filename)
-        count = 0
-        list_of_rows = []
-        for row in file_data:
-            if row['Gene Name'] == gene_name:
-                count += 1
+    file_data = parse_file(filename)
+    list_of_rows = []
+    for row in file_data:
+        if exact:
+            if value == row[col]:
                 list_of_rows.append(row)
-        yield (filename, list_of_rows)
+        else:
+            if value in row[col]:
+                list_of_rows.append(row)
+    return list_of_rows
 
 if __name__ == "__main__":
-    print 'Assignment 2:'
-    assignment_2()
-    print '\n\nAssignment 3:'
+    print 'Finding occurences of values under the "Annotation" column.', \
+          '(does not have to be exact match)'
+    assignment_2('Annotation', SEARCH_VALUES, FILENAMES)
+
+    print '\n\nFinding occurences of a gene under the "Gene Name" column.'
     user_input = raw_input('Enter a gene name: ')
-    assignment_3(user_input)
+    get_occurrences_of_gene(user_input, FILENAMES)
