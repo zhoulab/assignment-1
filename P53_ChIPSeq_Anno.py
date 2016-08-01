1081151dd5e6e4df75c9103994b2216e730e7da3
Victor Lin <vlin@ufl.edu>
2016-08-01 17:52:14 +0800

    Add AnnoFile object
    An object-oriented approach to organize variables being passed around
    (namely anno_file_rows and filepath info).
    Also merge generate_occurence_count_files() into do_anno_count().

--- P53_ChIPSeq_Anno.py
+++ P53_ChIPSeq_Anno.py
@@ -5,77 +5,132 @@ from collections import OrderedDict
 BASE_DIR = os.path.dirname(os.getcwd())
 
 
+class AnnoFile(object):
+    """Class for storing annotation file information
+
+    Attributes
+    ----------
+    filepath
+        absolute path of .anno file
+    filename
+        filename of .anno path
+    name
+        filename without extension ".anno"
+    folder
+        directory that `filename` is in
+    header
+        list of column names
+    rows
+        list of row dicts
+    """
+
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.filename = os.path.basename(filepath)
+        self.name = self.filename[:-5]
+        self.folder = os.path.basename(os.path.dirname(filepath))
+        self.parse()
+
+    def parse(self):
+        """Parse self.filepath to get header and rows"""
+        with open(self.filepath) as f:
+            reader = csv.reader(f, delimiter='\t')
+            self.header = ['PeakID' if 'PeakID' in col_name else col_name
+                           for col_name in reader.next()]
+            self.rows = [dict(zip(self.header, row))
+                         for row in reader]
+
+    def get_occurences(self, col, values, exact=True):
+        """Return iterator of row dicts containing matching `values` under `col`
+
+        Parameters
+        ----------
+        col : str
+        values : str or list
+        fpath : str
+        exact : bool
+            define whether occurences should partially contain
+            or match `values` exactly.
+        """
+        if type(values) is str:
+            values = [values]
+        for row in self.rows:
+            if exact:
+                if row[col] in values:
+                    yield row
+            else:
+                if any([val in row[col] for val in values]):
+                    yield row
+
+    def get_no_repeat_lines(self, desired_cols,
+                            search_col='Detailed Annotation',
+                            categories=None):
+        """Return `desired_cols` from lines that are not
+        in the repetitive sequence regions (repeats)
+
+        Parameters
+        ----------
+        categories : set of str
+            non-repeat Homer Genomic Annotation categories
+        desired_cols : iterable of str
+            columns to return
+        search_col : column that contains that contains 'repeats' information
+        """
+        if not categories:
+            categories = {'TSS', 'TTS', 'exon', "5' UTR", "3' UTR",
+                          'CpG', 'intron', 'Intergenic', 'non-coding'}
+        rows = self.get_occurences(search_col, categories)
+        for row in rows:
+            yield [row[col] for col in desired_cols]
+
+    def save_without_repeats(self, output_dir,
+                             desired_cols=None, save_name=None):
+        """Save a bed-format file with lines that are not
+        in the repetitive sequence regions (repeats)
+
+        Parameters
+        ----------
+        output_dir : filepath
+            path of diretory to save `save_name` in
+        """
+        if not desired_cols:
+            desired_cols = self.header
+        if not save_name:
+            save_name = self.filename
+        with open(os.path.join(output_dir, save_name), 'w') as f:
+            fwriter = csv.writer(f, delimiter='\t')
+            fwriter.writerow(desired_cols)
+            for line in self.get_no_repeat_lines(desired_cols):
+                fwriter.writerow(line)
+
+
 def create_dir(dirpath):
     """Return `dirpath` and create if it doesn't exist
-    **The directory `os.path.dirname(dirpath)` must already exist.
+    **Parent directory of `dirpath` must already exist.
     """
     if not os.path.exists(dirpath):
         os.makedirs(dirpath)
     return dirpath
 
 
-def parse_anno_file(fpath):
-    """Return each row in a dictionary format.
-    Use name 'PeakID' for column name rather than 'PeakID (...)'
+def prompt_for_folders(dirpath):
+    """Prompt user for list of folders from `dirpath`
 
-    Returns
-    -------
-    ({header1: value1-1, header2: value1-2, ...},
-     {header1: value2-1, header2: value2-2, ...}, ...)
-        Where header(j) is the jth header in the file and
-        value(i)-(j) is the value under the jth header and ith row in file.
+    Return list of folder names if all are valid subdirectories of `dirpath`,
+    otherwise `False` and display invalid inputs.
     """
-    with open(fpath) as f:
-        reader = csv.reader(f, delimiter='\t')
-        header = ['PeakID' if 'PeakID' in col_name else col_name
-                  for col_name in reader.next()]
-        for row in reader:
-            yield dict(zip(header, (value for value in row)))
-
-
-def generate_occurence_count_files(fpaths, col, values, output_dir,
-                                   plots_dir, val_colors):
-    """Write occurence count data into tab-delimited txt file.
-    Data is fetched from get_occurences and stored in `file_data`.
-
-    Parameters
-    ----------
-    fpaths : list of str
-        Filepaths of .anno files to search.
-        All must be under the same folder (see variable `folder`)
-    col : str
-        Column of .anno file to search under
-    values : list of str
-        Values to search for under `col`
-
-    Variables
-    ---------
-    folder : str
-        name of common directory amongst files in `fpaths`
-    files : list of str
-        filenames without trailing ".anno"
-    file_data : {fname1: OD{v1: count_v1, v2: count_v2, ...},
-                 fname2: OD{v1: count_v1, v2: count_v2, ...}, ...}
-    """
-    folder = os.path.basename(os.path.dirname(fpaths[0]))
-    files = [os.path.basename(path)[:-5] for path in fpaths]
-    with open(os.path.join(output_dir, folder + '-count.txt'), 'w') as file:
-        filewriter = csv.writer(file, delimiter='\t')
-        filewriter.writerow(['Values'] + files)
-        print 'Getting counts...'
-        file_data = {fname: OrderedDict() for fname in files}
-        for fpath, fname in zip(fpaths, files):
-            anno_file_rows = list(parse_anno_file(fpath))
-            for value in values:
-                file_data[fname][value] = len(list(get_occurences(anno_file_rows,
-                                              col, value, fpath, exact=False)))
-        for value in values:
-            filewriter.writerow([value] + [file_data[fname][value]
-                                           for fname in files])
-        filewriter.writerow(['Total'] + [sum(file_data[fname].values())
-                                         for fname in files])
-    print 'Creating pie plots...'
-    generate_pie_plots(file_data, values, val_colors, plots_dir)
+    all_folders = next(os.walk(dirpath))[1]
+    print 'Input subdirectories of {}'.format(dirpath)
+    folders = raw_input('Enter folder(s) to use, separated by ",": ')
+    folders = [f.strip() for f in folders.split(',')] if folders else []
+    if not folders:
+        print 'No folders specified.'
+    elif not all([f in all_folders for f in folders]):
+        invalid_folders_str = ', '.join([f for f in folders
+                                         if f not in all_folders])
+        print 'Some invalid directories ({})'.format(invalid_folders_str)
+        return False
+    return folders
 
 
 def generate_pie_plots(file_data, values, colors, plots_dir):
@@ -125,87 +180,26 @@ def generate_pie_plots(file_data, values, colors, plots_dir):
         plt.gcf().clear()
 
 
-def get_occurences(anno_file_rows, col, values, fpath, exact=True):
-    """Return rows containing matching `values` under `col`
-    Parameters
-    ----------
-    anno_file_rows : iterable of dict
-        see `parse_anno_file`
-    col : str
-    values : str or list
-    fpath : str
-    exact : bool
-        define whether occurences should partially contain
-        or match `values` exactly.
-
-    Returns
-    -------
-    generator of dicts
-    """
-    if type(values) is str:
-        values = [values]
-    for row in anno_file_rows:
-        if exact:
-            if row[col] in values:
-                yield row
-        else:
-            if any([val in row[col] for val in values]):
-                yield row
-
-
-def get_no_repeat_lines(anno_file_rows, fpath, desired_cols,
-                        search_col='Detailed Annotation',
-                        categories=None):
-    """
-    Return `desired_cols` from lines that are not
-    in the repetitive sequence regions (repeats)
-
-    Parameters
-    ----------
-    anno_file_rows : iterable of dict
-        see `parse_anno_file`
-    fpath : str
-        filepath of .anno file
-    categories : set of str
-        non-repeat Homer Genomic Annotation categories
-    desired_cols : list
-        columns to return
-    search_col : column that contains that contains 'repeats' information
-    """
-    if not categories:
-        categories = {'TSS', 'TTS', 'exon', "5' UTR", "3' UTR",
-                     'CpG', 'intron', 'Intergenic', 'non-coding'}
-    rows = get_occurences(anno_file_rows, search_col, categories, fpath)
-    for row in rows:
-        yield [row[col] for col in desired_cols]
-
-
-def prompt_for_folders(dirpath):
-    """Prompt user for list of folders from `dirpath`.
-
-    Return list of folder names if all are valid subdirectories of `dirpath`,
-    otherwise `False` and display invalid inputs.
-    """
-    all_folders = next(os.walk(dirpath))[1]
-    print 'Input subdirectories of {}'.format(dirpath)
-    folders = raw_input('Enter folder(s) to use, separated by ",": ')
-    folders = [f.strip() for f in folders.split(',')] if folders else []
-    if not folders:
-        print 'No folders specified.'
-    elif not all([f in all_folders for f in folders]):
-        invalid_folders_str = ', '.join([f for f in folders
-                                         if f not in all_folders])
-        print 'Some invalid directories ({})'.format(invalid_folders_str)
-        return False
-    return folders
-
-
 def do_anno_count():
     """Generate count files and pie plots with pre-defined parameters.
+
+    Write occurence count data into tab-delimited txt file.
+    Data is fetched from get_occurences and stored in `file_data`.
+
     Variables
     ---------
     folders : list of str
         sample folders to use (under data/Annos)
+    search_values : list of str
+    val_colors : list of str
+        hex colors for corresponding element in ordered `values`
+    output_dir : filepath (str)
+        directory to save all results
+    plots_dir : filepath (str)
+        directory to save plot JPEGs
+        create subdirectories for each anno folder
+    file_data : dict
+        see `generate_occurence_counts()`
     """
     annos_dir_path = os.path.join(BASE_DIR, 'data/Annos')
     folders = prompt_for_folders(annos_dir_path)
@@ -218,20 +212,37 @@ def do_anno_count():
     output_dir = create_dir(os.path.join(BASE_DIR, 'results',
                                          'P53-ChIPSeq-Anno-count'))
     plots_dir = create_dir(os.path.join(output_dir, 'plots'))
-
-    for anno_subdir_name in folders:
-        anno_subdir_path = os.path.join(annos_dir_path, anno_subdir_name)
+    for anno_folder in folders:
+        anno_subdir_path = os.path.join(annos_dir_path, anno_folder)
         plots_subdir = create_dir(os.path.join(plots_dir,
-                                               anno_subdir_name + '-count_plots'))
+                                               anno_folder + '-count_plots'))
         print ('{}: finding occurences of values under the '
-               '"Annotation" column... ').format(anno_subdir_name)
+               '"Annotation" column... ').format(anno_folder)
         anno_fpaths = [os.path.join(anno_subdir_path, f)
                        for f in next(os.walk(anno_subdir_path))[2]
                        if '.anno' in f]
-        generate_occurence_count_files(anno_fpaths, 'Annotation',
-                                       search_values, output_dir,
-                                       plots_subdir, val_colors)
-        print 'Done. Saved at: "{}"'.format(anno_subdir_name + '-count.txt')
+        annos = [AnnoFile(fpath) for fpath in anno_fpaths]
+        fnames = [anno_obj.name for anno_obj in annos]
+        folder = annos[0].folder
+        with open(os.path.join(output_dir, folder + '-count.txt'), 'w') as f:
+            filewriter = csv.writer(f, delimiter='\t')
+            filewriter.writerow(['Values'] + fnames)
+            print 'Getting counts...',
+            file_data = {fname: OrderedDict() for fname in fnames}
+            for anno_obj in annos:
+                for val in search_values:
+                    file_data[anno_obj.name][val] = len(list(anno_obj.get_occurences(
+                                                             'Annotation', val,
+                                                             exact=False)))
+            for value in search_values:
+                filewriter.writerow([value] + [file_data[fname][value]
+                                               for fname in fnames])
+            filewriter.writerow(['Total'] + [sum(file_data[fname].values())
+                                             for fname in fnames])
+        print 'done. Saved at: "{}"'.format(anno_folder + '-count.txt')
+        print 'Creating pie plots...',
+        generate_pie_plots(file_data, search_values, val_colors, plots_subdir)
+        print 'done. Saved at: "{}"'.format(plots_subdir)
 
 
 def do_remove_repeats():
@@ -257,16 +268,15 @@ def do_remove_repeats():
         output_subdir = create_dir(os.path.join(output_dir,
                                                 anno_subdir_name +
                                                 '-no_repeats'))
-        files = next(os.walk(anno_subdir_path))[2]
-        files = [f[:-5] for f in files]  # remove .anno from filenames
-        for fname in files:
-            fpath = os.path.join(anno_subdir_path, fname + '.anno')
-            print 'Processing {}...'.format(fname)
-            anno_file_rows = list(parse_anno_file(fpath))
-            out_file = os.path.join(output_subdir, fname + '-no_repeats.bed')
-            with open(out_file, 'w') as f:
-                fwriter = csv.writer(f, delimiter='\t')
-                fwriter.writerow(output_cols)
-                for line in get_no_repeat_lines(anno_file_rows,
-                                                fpath, output_cols):
-                    fwriter.writerow(line)
+        files = [fname for fname in next(os.walk(anno_subdir_path))[2]
+                 if '.anno' in fname]
+        fpaths = [os.path.join(anno_subdir_path, fname)
+                  for fname in files]
+        print fpaths
+        for fpath in fpaths:
+            anno_obj = AnnoFile(fpath)
+            print 'Processing {}...'.format(anno_obj.name)
+            out_file = anno_obj.name + '-no_repeats.bed'
+            anno_obj.save_without_repeats(output_subdir,
+                                          desired_cols=output_cols,
+                                          save_name=out_file)
